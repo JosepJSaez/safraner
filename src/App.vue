793d317b4b2cad44b6e2533871c7294751ea59f5
Safra <safraneah@gmail.com>
2023-02-13 17:20:00 +0100

    Adición de componente CARDS

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,53 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import CardItem from "./components/codingItems.vue";
+export default {
+    name: "App", 
+    components: {
+        CardItem
+    }
+}
 </script>
 
 <template>
-  <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div class="container">
+      <CardItem titul="HTML5+CSS3" :subtitles="0"> Lorem ipsum dolor sit amet consectetur 
+            adipisicing elit. Perspiciatis placeat et, similique 
+            exercitationem nulla odit officiis. Porro nostrum ratione
+            quibusdam nisi incidunt vitae, quo dignissimos esse officiis 
+            explicabo! Quae, veniam. </CardItem>
+      <CardItem titul="PHP" :subtitles="1"/>
+      <CardItem titul="JavaScript" :subtitles="2" />
+      <CardItem titul="Vue.JS" :subtitles="3" />
+      <CardItem titul="SQL" :subtitles="4" />
+    </div>
 </template>
 
-<style scoped>
+<style>
+
 .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
-  transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
+
+.container {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  padding: 10px 10px;
+}
+
 </style>
---
+++ src/components/codingItems.vue
@@ -0,0 +1,96 @@
+<script>
+
+const definedSubtitles = ["Trucos de maquetación web",
+"PHP & Laravel, para el backend",
+"JavaScript, el mejor lenguaje",
+"Vue.JS, el framework comunitario",
+"SQL para atacar a las bases de datos"
+]
+
+export default {
+    name : "CardItem",
+    props:{
+        titul: {
+            type: String, 
+            default: "Gatard moniko",
+            required: false,
+            validator: titulo => titulo.length > 5
+        },
+        subtitles: {
+            type: Number, 
+            required: false,
+            validator: val => 0 && val < subtitles.length
+        }
+    },
+    data() {
+        return{
+            subtitulosDatos : definedSubtitles[this.subtitles]
+
+        }
+    },
+    computed : {},
+    methods: {}
+};
+</script>
+
+<template>
+    <div class="card">       
+    <img src="../assets/cat.jpg" alt="gatard">  
+        <div class="content">            
+            <h1> {{ titul }}</h1>
+            <h2>{{ subtitulosDatos }}</h2>
+            <slot></slot>
+        </div>  
+    </div>
+</template>
+
+<style scoped>
+@font-face {
+    font-family: 'Montserrat';
+    src: url('../../public/fonts/Montserrat-vf.ttf') format("truetype"),
+    url('../../public/fonts/Montserrat-It-vf.ttf') format("truetype");
+}
+.card {   
+    margin: 10px;
+    max-height: 600px;
+    max-width: 350px;
+    border: 1px solid black;
+    box-shadow: 4px 4px 10px #aaa;
+    
+}
+
+.card img {    
+    
+width: 100%;
+height: 250px;
+box-shadow: 6px 6px 10px rgb(46, 164, 131);
+}
+
+p{
+    color: beige;
+    display: flex;
+}
+
+.content {
+   
+    padding: 20px;
+    border-radius: 10px;     
+}
+
+
+h1 {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 2rem;
+    text-align: center;
+    margin-top: 2rem;
+    color: violet;
+}
+h2 {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 1.5rem;
+    text-align: center;
+    margin-top: 1.5rem;
+    color: rgb(144, 135, 209);
+}
+
+</style>
