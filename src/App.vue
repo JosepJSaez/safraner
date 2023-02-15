769eb229a8b36aa07e0938532d2e56dd0d46e5c3
Safra <safraneah@gmail.com>
2023-02-16 00:13:06 +0100

    Hamburguer menu Unfinished

--- src/App.vue
+++ src/App.vue
@@ -4,27 +4,65 @@ export default {
     name: "App", 
     components: {
         CardItem
+    },
+    data() {
+      return {
+        showMenu: false
+      }
+    },
+    methods: {
+      toggleNavbar: function(){
+        this.showMenu = !this.showMenu;
+      }
     }
 }
 </script>
 
 <template>
-  <header class="fixed top-0 left-0 w-full h-16 bg-red-200 flex justify-end " >
-    <nav class="relative inline-block bg-blue-200 h-8 top-4 ">
-      <ul class="flex flex-row " >
-        <li><a href="#">Timeline</a></li>
-        <li><a href="#">Experiencia</a></li>
-        <li><a href="#">Sobre mí</a></li>
-        <li><a href="#">Contacto</a></li>
-        <li><a href="#">Blog</a></li>
-      </ul>
+  <header class="fixed top-0 left-0 w-full h-16 bg-red-200 flex justify-center " >
+    <nav class="p-3 border-gray-200  bg-gray-50 dark:bg-gray-800 dark:border-gray-700 w-full" >
+      <div class="container flex flex-wrap items-center justify-between">
+        <a href="#" class="flex items-center">
+            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Logo</span>
+        </a>
+
+        <button class="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none" type="button" v-on:click="toggleNavbar()">
+          <span class="sr-only">Open main menu</span>
+          <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+        </button>
+
+        <div v-bind:class="{'hidden': !showMenu, 'flex': showMenu}" class="lg:flex lg:flex-grow items-center">
+        
+          <ul class="flex flex-col mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Home</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Timeline</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Experiencia</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sobre mí</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contacto</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Blog</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
     </nav>
   </header>
   
   <section></section>
   <footer></footer>
 
-    <!-- <div class="container">
+    <!-- <div class="grupo">
       <CardItem image="images/htmlcss.jpg" titul="HTML5+CSS3" :subtitles="0"></CardItem>
       <CardItem image="images/php.jpg" titul="PHP" :subtitles="1"/>
       <CardItem image="images/javascript.jpg" titul="JavaScript" :subtitles="2" />
@@ -36,16 +74,12 @@ export default {
 
 <style scoped>
 
-.container {
+.grupo {
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
   padding: 10px 10px;
 }
 
-li{
-  padding-right: 7px;
-  
-}
 
 </style>
