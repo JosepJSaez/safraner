5f0a9cb33cdccda2dfdd768bc9caabb07477997e
Josep Sáez <josejavier.saez@inelcom.es>
2023-02-16 13:44:14 +0100

    Burguer menu finished

--- src/App.vue
+++ src/App.vue
@@ -11,55 +11,54 @@ export default {
       }
     },
     methods: {
-      toggleNavbar: function(){
-        this.showMenu = !this.showMenu;
-      }
     }
 }
 </script>
 
 <template>
-  <header class="fixed top-0 left-0 w-full h-16 bg-red-200 flex justify-center " >
-    <nav class="p-3 border-gray-200  bg-gray-50 dark:bg-gray-800 dark:border-gray-700 w-full" >
-      <div class="container flex flex-wrap items-center justify-between">
-        <a href="#" class="flex items-center">
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Logo</span>
-        </a>
 
-        <button class="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none" type="button" v-on:click="toggleNavbar()">
-          <span class="sr-only">Open main menu</span>
-          <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-        </button>
-
-        <div v-bind:class="{'hidden': !showMenu, 'flex': showMenu}" class="lg:flex lg:flex-grow items-center">
-        
-          <ul class="flex flex-col mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
-            <li>
-              <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Home</a>
-            </li>
-            <li>
-              <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Timeline</a>
-            </li>
-            <li>
-              <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Experiencia</a>
-            </li>
-            <li>
-              <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sobre mí</a>
-            </li>
-            <li>
-              <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contacto</a>
-            </li>
-            <li>
-              <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Blog</a>
-            </li>
-          </ul>
+  <div class="fixed top-0 left-0 w-full h-16 bg-red-200 flex justify-center " >
+    <nav  class=" bg-gray-200  container  px-6  py-8  mx-auto  md:flex md:justify-between md:items-center">
+      
+        <div class="flex items-center justify-between">
+          <router-link to="/" class=" text-xl font-bold text-gray-800 md:text-2xl hover:text-blue-400 " >Logo</router-link>
+          <!-- Mobile menu button -->
+          <div @click="showMenu = !showMenu" class="flex md:hidden">
+            <button type="button" class=" text-gray-800 hover:text-gray-400 focus:outline-none focus:text-gray-400">
+              <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+                <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
+              </svg>
+            </button>
+          </div>
         </div>
-      </div>
 
+        <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+        <ul :class="showMenu ? 'flex' : 'hidden'" class="bg-yellow-300 flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0">
+          <li class="text-sm font-bold text-yellow-800 hover:text-blue-400">
+            Home
+          </li>
+          <li class="text-sm font-bold text-yellow-800 hover:text-blue-400">
+            About
+          </li>
+          <li class="text-sm font-bold text-yellow-800 hover:text-blue-400">
+            Blogs
+          </li>
+          <li class="text-sm font-bold text-yellow-800 hover:text-blue-400">
+            Contact Us
+          </li>
+        </ul>
     </nav>
+  </div>
+  <header>
+
+
   </header>
   
-  <section></section>
+  <section>
+
+    
+
+  </section>
   <footer></footer>
 
     <!-- <div class="grupo">
@@ -73,6 +72,12 @@ export default {
 </template>
 
 <style scoped>
+
+@font-face {
+    font-family: 'Montserrat';
+    src: url('../public/fonts/Montserrat-vf.ttf') format("truetype"),
+    url('../public/fonts/Montserrat-It-vf.ttf') format("truetype");
+}
 
 .grupo {
   display: flex;
