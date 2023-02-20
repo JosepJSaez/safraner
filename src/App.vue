6970f118a9d599cd40b1e4a07922b690e3c39913
Josep Sáez <josejavier.saez@inelcom.es>
2023-02-20 14:37:42 +0100

    Estructura HOME 1

--- src/App.vue
+++ src/App.vue
@@ -16,11 +16,12 @@ export default {
 </script>
 
 <template>
+<div class="contenedor" >
 
   <div class="fixed top-0 left-0 w-full h-16 bg-red-200 flex justify-center " >
-    <nav  class=" bg-gray-200  container  px-6  py-8  mx-auto  md:flex md:justify-between md:items-center">
+    <nav  class="bg-gray-200 container  px-6  py-8  mx-auto  md:flex md:justify-between md:items-center">
       
-        <div class="flex items-center justify-between">
+        <div class="flex items-center justify-between ">
           <router-link to="/" class=" text-xl font-bold text-gray-800 md:text-2xl hover:text-blue-400 " >Logo</router-link>
           <!-- Mobile menu button -->
           <div @click="showMenu = !showMenu" class="flex md:hidden">
@@ -33,42 +34,103 @@ export default {
         </div>
 
         <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
-        <ul :class="showMenu ? 'flex' : 'hidden'" class="bg-yellow-300 flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0">
+        <ul :class="showMenu ? 'flex' : 'hidden'" class="bg-yellow-300 flex-col mt-1 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0">
           <li class="text-sm font-bold text-yellow-800 hover:text-blue-400">
-            Home
+            <button type="button" class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 ">Home</button>
           </li>
           <li class="text-sm font-bold text-yellow-800 hover:text-blue-400">
-            About
+            <button type="button" class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 ">Sobre mi</button>
           </li>
           <li class="text-sm font-bold text-yellow-800 hover:text-blue-400">
-            Blogs
+            <button type="button" class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 ">IT Blog</button>
           </li>
           <li class="text-sm font-bold text-yellow-800 hover:text-blue-400">
-            Contact Us
+            <button type="button" class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 ">Contáctame</button>
           </li>
         </ul>
     </nav>
   </div>
-  <header>
 
-
+  <header class="w-full mt-16 h-52 bg-purple-500">
+    <h1>capçalera</h1>
   </header>
   
-  <section>
+  <section class="w-full min-h-auto bg-orange-500 px-64 py-32 flex flex-col">
+
+      <div class="w-full bg-red-500 ">
+      <h3> futer</h3>
+    </div>
+    <div class="w-full bg-red-500 ">
+      <h3> futer</h3>
+    </div> <div class="w-full bg-red-500 ">
+      <h3> futer</h3>
+    </div> <div class="w-full bg-red-500 ">
+      <h3> futer</h3>
+    </div> <div class="w-full bg-red-500 ">
+      <h3> futer</h3>
+    </div> <div class="w-full bg-red-500 ">
+      <h3> futer</h3>
+    </div> <div class="w-full bg-red-500 ">
+      <h3> futer</h3>
+    </div> <div class="w-full bg-red-500 ">
+      <h3> futer</h3>
+    </div> <div class="w-full bg-red-500 "> 
+      <h3> futer</h3>
+    </div> <div class="w-full bg-red-500 ">
+      <h3> futer</h3>
+    </div> <div class="w-full bg-red-500 ">
+      <h3> futer</h3>
+    </div> <div class="w-full bg-red-500 ">
+      <h3> futer</h3>
+    </div> <div class="w-full bg-red-500 ">
+      <h3> futer</h3>
+    </div> <div class="w-full bg-red-500 ">
+      <h3> futer</h3>
+    </div> <div class="w-full bg-red-500 ">
+      <h3> futer</h3>
+    </div> <div class="w-full bg-red-500 ">
+      <h3> futer</h3>
+    </div> <div class="w-full bg-red-500 ">
+      <h3> futer</h3>
+    </div>
+    <div class="w-full bg-red-500 w-full">
+      <h3> futer</h3>
+    </div>
+    <div class="w-full bg-red-500 ">
+      <h3> futer</h3>
+    </div>
+    <div class="w-full bg-red-500 ">
+      <h3> futer</h3>
+    </div>
+    <div class="w-full bg-red-500 ">
+      <h3> kliiiter</h3>
+    </div>
+  </section>
+
+  <footer class="w-full bg-cyan-500 min-h-auto p-14">
+    
+    <div class="w-full min-h-auto bg-orange-500 flex flex-row  justify-between items-center">
+      <a>HACHEREF</a>
+      <a>HACHEREF</a>
+      <a>HACHEREF</a>
+      <a>HACHEREF</a>
+    </div>
+  
+    <h2>Redes sociales - footer</h2>
+    <h2>Redes sociales - footer</h2>
+    <h2>Redes sociales - footer</h2>
+  </footer>
+
 
     
-
-  </section>
-  <footer></footer>
-
-    <!-- <div class="grupo">
-      <CardItem image="images/htmlcss.jpg" titul="HTML5+CSS3" :subtitles="0"></CardItem>
+    <!--  <CardItem image="images/htmlcss.jpg" titul="HTML5+CSS3" :subtitles="0"></CardItem>
       <CardItem image="images/php.jpg" titul="PHP" :subtitles="1"/>
       <CardItem image="images/javascript.jpg" titul="JavaScript" :subtitles="2" />
       <CardItem image="images/vue.jpg" titul="Vue.JS" :subtitles="3" />
       <CardItem image="images/mysql.jpg" titul="MySQL" :subtitles="4" />
       <CardItem image="images/responsive.jpg" titul="Responsive Design" :subtitles="5" />
-    </div> -->
+      -->
+    </div> 
 </template>
 
 <style scoped>
@@ -79,11 +141,11 @@ export default {
     url('../public/fonts/Montserrat-It-vf.ttf') format("truetype");
 }
 
-.grupo {
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-  padding: 10px 10px;
+
+.contenedor {
+  width: 100%;
+  position: relative;
+  background-color: blanchedalmond;
 }
 
 
