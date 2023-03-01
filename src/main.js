f590bb8647c71868e0d6192ba6bee22a84a17df9
Josep Sáez <josejavier.saez@inelcom.es>
2023-03-01 09:55:24 +0100

    TIMELINE OK 1st TEST

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import App from './App.vue'
 import './index.css'
 import router from "./router/router";
 
+import { plugin } from 'vue3-cute-component'
+import 'vue3-cute-component/dist/style.css'
+
+
 createApp(App)
+.use(plugin)
 .use(router)
 .mount("#app");
--- src/views/sobreJsaez.vue
+++ src/views/sobreJsaez.vue
@@ -1,9 +1,22 @@
-<script>
-    export default {
-        name: "Sobremi",
-    };
+<script lang="ts">
+    import { Timeline, TimelineTitle, TimelineItem } from 'vue3-cute-component'
 </script>
 
 <template>
-    <h1>sobremi</h1>
+
+<Timeline theme="lightblue">
+    <TimelineTitle>2022</TimelineTitle>
+    <timeline-item bg-color="red" font-color="blue">TEST</timeline-item>
+    <timeline-item :hollow="true" bg-color="blue">TEST</timeline-item>
+    <timeline-item bg-color="red" font-color="blue">TEST</timeline-item>
+    <timeline-item>
+  <template #others>
+    <img
+      src="https://user-images.githubusercontent.com/12069729/36057805-80cfc3d2-0e4e-11e8-8851-6fda091ff389.png"
+      class="icon-heart"
+  />
+  </template>
+</timeline-item>
+</Timeline>
+
 </template>
