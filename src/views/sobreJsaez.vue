9cc2371651557ab31cdbb79f91949ba5425a240d
Josep Sáez PC Casa <safraneah@gmail.com>
2023-03-03 00:03:28 +0100

    STYILING TIMELINE POR ACABAR ( .css del module )

--- src/views/sobreJsaez.vue
+++ src/views/sobreJsaez.vue
@@ -3,20 +3,22 @@
 </script>
 
 <template>
-
-<Timeline theme="lightblue">
-    <TimelineTitle>2022</TimelineTitle>
-    <timeline-item bg-color="red" font-color="blue">TEST</timeline-item>
-    <timeline-item :hollow="true" bg-color="blue">TEST</timeline-item>
-    <timeline-item bg-color="red" font-color="blue">TEST</timeline-item>
-    <timeline-item>
-  <template #others>
-    <img
-      src="https://user-images.githubusercontent.com/12069729/36057805-80cfc3d2-0e4e-11e8-8851-6fda091ff389.png"
-      class="icon-heart"
-  />
-  </template>
-</timeline-item>
-</Timeline>
-
+  
+  <div class="flex flex-wrap place-content-center justify-center">
+    <Timeline theme="lightblue" class="w-2/5" >
+        <!-- <TimelineTitle>2022</TimelineTitle> -->
+        <timeline-item bg-color="red" font-color="blue">TEST</timeline-item>
+        <timeline-item :hollow="true" bg-color="blue">TEST</timeline-item>
+        <timeline-item bg-color="red" font-color="blue">TEST</timeline-item>
+        <timeline-item :hollow="true" bg-color="blue">TEST</timeline-item>
+        <timeline-item bg-color="red" font-color="blue">TEST</timeline-item>
+        <timeline-item :hollow="true" bg-color="blue">TEST</timeline-item>
+        <timeline-item bg-color="red" font-color="blue">TEST</timeline-item>
+        <timeline-item :hollow="true" bg-color="blue">TEST</timeline-item>
+        <timeline-item bg-color="red" font-color="blue">TEST</timeline-item>
+        <timeline-item :hollow="true" bg-color="blue">TEST</timeline-item>
+        <timeline-item bg-color="red" font-color="blue">TEST</timeline-item>
+    </Timeline>
+  </div>
+  
 </template>
