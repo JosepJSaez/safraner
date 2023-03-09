2d9b23cb8385dbf0c76e86371de2a7c663fbe37c
Josep Sáez <josejavier.saez@inelcom.es>
2023-03-09 14:51:10 +0100

    Correcion de estilos importante OK

--- src/components/header.vue
+++ src/components/header.vue
@@ -13,9 +13,9 @@ export default {
 
 <template>
 
-    <div class="fixed top-0 left-0 w-full h-16 bg-red-200 flex justify-center " >
+    <div class="fixed z-50 top-0 left-0 w-full h-16 bg-red-200 flex justify-center " >
 
-        <nav  class="bg-gray-200 container  px-6  py-8  mx-auto  md:flex md:justify-between md:items-center">
+        <nav  class="bg-gray-200 container  md:px-6  py-8  mx-auto  md:flex md:justify-between md:items-center">
             <div class="flex items-center justify-between ">
                 <router-link to="/" class=" text-xl font-bold text-gray-800 md:text-2xl hover:text-blue-400 " >Logo</router-link>
                 <!-- Mobile menu button -->
@@ -29,7 +29,7 @@ export default {
             </div>
 
             <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
-            <ul :class="showMenu ? 'flex' : 'hidden'" class="bg-yellow-300 flex-col mt-1 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0">
+            <ul :class="showMenu ? 'flex' : 'hidden'" class="bg-yellow-300 md:bg-yellow-700 flex-col mt-1 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0">
                 <li class="text-sm font-bold text-yellow-800 hover:text-blue-400">
                     <router-link to="/"><button type="button" class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 ">Home</button></router-link>
                 </li>
