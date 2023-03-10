c2e9b1e4b7c386c1bf3b8ca764ca87b6e9e32b52
Josep Sáez <josejavier.saez@inelcom.es>
2023-03-10 14:56:15 +0100

    Header completamente arreglada

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -12,16 +12,17 @@ export default {
 
     <footer class="w-full bg-cyan-500 min-h-auto p-14">
     
-        <div class="w-full min-h-auto bg-orange-500 flex flex-row  justify-between items-center">
+        <div class="w-full min-h-auto bg-orange-500 flex flex-row  justify-around items-center mb-6">
             <a>HACHEREF</a>
             <a>HACHEREF</a>
             <a>HACHEREF</a>
             <a>HACHEREF</a>
         </div>
 
-        <h2>Redes sociales - footer</h2>
-        <h2>Redes sociales - footer</h2>
-        <h2>Redes sociales - footer</h2>
+        
+        <h2> Josep J. Sáez - 2023</h2>
+        <p>Developed with <g-emoji class="g-emoji" alias="heart" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/2764.png">❤️</g-emoji> and <g-emoji class="g-emoji" alias="coffee" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/2615.png">☕️</g-emoji></p>
+
 
     </footer>
 
